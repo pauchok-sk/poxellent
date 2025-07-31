99657476ec59bfee783094ed97ddcf575b99ae3d
pauchok-sk <pauchoooooook@gmail.com>
2025-07-31 10:35:39 +0300

    full

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -6,9 +6,9 @@ export default function sliders() {
       speed: 800,
       spaceBetween: 16,
       slidesPerView: 1,
-      // autoplay: {
-      //   delay: 3000
-      // },
+      autoplay: {
+        delay: 3000,
+      },
       pagination: {
         el: ".s-team .slider-pagination",
         clickable: true,
@@ -21,6 +21,29 @@ export default function sliders() {
         630: {
           spaceBetween: 16,
           slidesPerView: 2,
+        },
+      },
+    });
+  }
+
+  const reviewsSlider = document.querySelector(".s-reviews__slider");
+
+  if (reviewsSlider) {
+    const swiper = new Swiper(reviewsSlider, {
+      speed: 800,
+      spaceBetween: 24,
+      slidesPerView: "auto",
+      // autoplay: {
+      //   delay: 3200,
+      // },
+      pagination: {
+        el: ".s-reviews .slider-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          spaceBetween: 32,
+          slidesPerView: "auto",
         },
       },
     });
